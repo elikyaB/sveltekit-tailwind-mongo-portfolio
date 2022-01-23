571d91eafff91250cad34912cb83b7ae9848bad7
elikya <elikyabokanga@gmail.com>
2022-01-22 23:35:33 -0500

    initializing components

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { onMount } from 'svelte'
+
+    let postHero = false
+    function fixNavbar(){
+        if (window.scrollY>screen.height) {postHero = true}
+        else {postHero=false}
+        setTimeout(() => {fixNavbar()}, 20)
+    }
+
+    onMount(() => fixNavbar())
+
+    let active = false
+    function activate(){active = !active}
+</script>
+
+<nav id="navi" class="bg-blue-500">
+    <div class="">
+        <a href="/" class="">Eli B3 | Web Dev</a>
+        <!-- <div class="">Test</div> -->
+        <div 
+            class=""
+            on:click|stopPropagation={activate}
+            on:pointerenter={activate}
+            on:pointerleave={activate}
+        >
+        <!-- need new burger menu icon -->
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </div>
+    </div>
+    <div class="">
+        <div class="">
+            <!-- <div class="">... pulsing dot bridge ...</div> -->
+        </div>
+        <div class="">
+            <a href="#about" class="">About</a>
+            <a href="#portfolio" class="">Portfolio</a>
+            <a href="#contact" class="">Contact</a>
+            <a href="#about" class="">Resume</a>
+            <a href="#portfolio" class="">Github</a>
+            <!-- <a href="#contact" class="">Blog (Coming Soon)</a> -->
+        </div>
+    </div>
+</nav>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Eli B3 Web Dev</title>
+	<style>
+		@import 'tailwindcss/base';
+		@import 'tailwindcss/components';
+		@import 'tailwindcss/utilities';
+		@import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap');
+	</style>
+</svelte:head>
+
+<script>
+	// import '../app.css'
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+</script>
+
+<slot/>
+<Header />
+<main>
+	<slot/>
+</main>
+<Footer />
